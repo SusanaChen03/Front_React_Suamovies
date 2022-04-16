7969d4bbe8dca1b2f475df857c069af229212987
SusanaChen03 <grupochen@hotmail.com>
2022-04-16 09:09:28 +0200

    html for register do it

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './css/bootstrap.min.css';
 import 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js'
 import Header from './components/Header/Header';
 import Login from './containers/Login/Login';
+import Register from './containers/Register/Register.js';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      <Login></Login>
+     
+      <Register></Register>
     </div>
   );
 }
