932a41ba938d85bea1c4c04e2ba7ba6bb3e282e6
SusanaChen03 <grupochen@hotmail.com>
2022-04-16 10:08:30 +0200

    test carousel with images un HomeUser

--- src/App.js
+++ src/App.js
@@ -5,14 +5,12 @@ import Header from './components/Header/Header';
 import Login from './containers/Login/Login';
 import Register from './containers/Register/Register.js'
 import MovieCard from './components/MovieCard/MovieCard.js'
+import HomeUser from './containers/HomeUser/HomeUser';
 
 function App() {
   return (
     <div className="App">
-    
-    
-  
-      <MovieCard></MovieCard>
+    <HomeUser></HomeUser>
     </div>
   );
 }
--- src/containers/HomeUser/HomeUser.js
+++ src/containers/HomeUser/HomeUser.js
@@ -1,21 +1,31 @@
+import './HomeUser.css';
+import images from '../../images/deadpool.jpg'
+import Moviecard from '../../components/MovieCard/MovieCard.js';
+
+
+
 const HomeUser = () => {
   return (
     <div>
+
       <div  id="carouselExampleSlidesOnly" class="carousel slide"   data-bs-ride="carousel" >
         <div class="carousel-inner">
           <div class="carousel-item active">
-            <img src="..." class="d-block w-100" alt="..." />
+            <img src={images} class="d-block w-100" alt="images1" />
           </div>
           <div class="carousel-item">
-            <img src="..." class="d-block w-100" alt="..." />
+            <img src={images} class="d-block w-100" alt="images2" />
           </div>
           <div class="carousel-item">
-            <img src="..." class="d-block w-100" alt="..." />
+            <img src={images} class="d-block w-100" alt="images3" />
           </div>
         </div>
+        <Moviecard></Moviecard>
+
+
       </div>
 
-      
+
     </div>
   );
 };
