a741027df062b0331de817ae6a16469ee9a0e64a
SusanaChen03 <grupochen@hotmail.com>
2022-04-16 09:57:22 +0200

    structure MovieCard ok

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js'
 import Header from './components/Header/Header';
 import Login from './containers/Login/Login';
 import Register from './containers/Register/Register.js'
+import MovieCard from './components/MovieCard/MovieCard.js'
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
     
-      <Register></Register>
+    
+  
+      <MovieCard></MovieCard>
     </div>
   );
 }
--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,21 +1,22 @@
-const MovieCard = ( ) => {
+import "./MovieCard.css";
 
+const MovieCard = () => {
+  return (
+    <div className="movieCard">
+      <img src="..." class="card-img-top" alt="..." />
 
-
-
-
-    return(
-        <div className=" movieCard">
-            <li className="movieName">Película: {}</li>
-            <li>Año: {}</li>
-            <li>Género: {} </li>
-            <li>Actores: {} </li>
-            <li>Precio: {} </li>
-            <li>Tiempo de alquiler: {} </li>
-            <li>Estado: {} </li>                  /*estado para cuando este alquilado*/
-            <button>Alquilar</button>
-        </div>
-    )
+      <h5 class="card-title">Movie name {}</h5>
+      <li>Año: {}</li>
+      <li>Género: {} </li>
+      <li>Actores: {} </li>
+      <li>Precio: {} </li>
+      <li>Tiempo de alquiler: {} </li>
+      <li>Estado: {} </li>
+      <a href="#" class="btn btn-primary">Alquilar </a>
+    </div>
+  );
 };
 
 export default MovieCard;
+
+/*<button type="button" class="btn btn-info"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Información</font></font></button>*/
