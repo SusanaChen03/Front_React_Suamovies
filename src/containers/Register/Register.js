996a7865beaeb285b60975049d4d3e4fd4a60b09
SusanaChen03 <grupochen@hotmail.com>
2022-04-17 09:57:39 +0200

    html register finish

--- src/containers/Register/Register.js
+++ src/containers/Register/Register.js
@@ -5,26 +5,25 @@ const Register = () => {
     <div className="register">
       <form className="containerRegister">
         <fieldset>
-          <h1>Registro</h1>
+          <h1>REGISTRO</h1>
 
           <div class="mb-3">
-            <label for="disabledTextInput" class="form-label">Nombre</label>
-            <input type="text" id="text" class="form-control"  placeholder="Disabled input"  />
+            <label for="textInput" class="form-label">Nombre</label>
+            <input type="text" id="text" class="form-control"  placeholder="Introduce tu nombre"  />
           </div>
 
           <div class="mb-3">
-            <label for="disabledTextInput" class="form-label">E-mail</label>
-            <input type="email" id="email" class="form-control"  placeholder="Disabled input"  />
+            <label for="textInput" class="form-label">E-mail</label>
+            <input type="email" id="email" class="form-control"  placeholder="Introduce tu correo favorito"  />
           </div>
 
           <div class="mb-3">
-            <label for="disabledTextInput" class="form-label">Contraseña</label>
-            <input type="password" id="password" class="form-control"  placeholder="Disabled input"  />
+            <label for="textInput" class="form-label">Contraseña</label>
+            <input type="password" id="password" class="form-control"  placeholder="Introduce una contraseña"  />
           </div>
 
           <div class="mb-3">
             <label for="Select" class="form-label">Disabled select menu </label>
-              
             <select id="disabledSelect" class="form-select">
               <option>Disabled select</option>
             </select>
@@ -35,7 +34,7 @@ const Register = () => {
       Al crear la cuenta, aceptas los Términos y Condiciones, la Política de protección de datos y la Política de cookies. 
       Suamovies va a tratar tus datos para poder prestarte sus servicios.
       </p>
-      <button type="submit" class="btn btn-primary">Crear Cuenta </button>
+      <button type="submit" class="btn btn-primary"> Crear Cuenta </button>
     </div>
   );
 };
